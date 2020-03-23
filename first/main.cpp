69ed4050502e1f87b93f25917f05b7d6ec7cc235
DanyArctic <flip4477@gmail.com>
2020-03-24 00:14:29 +0300

    my first commit

---
+++ first/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    int e =5;
+    std::cout << e << std::endl;
+    std::cin >> e;
+    std::cout << e << std::endl;
+    return 0;
+}
