2695c07fd3dca25d18d8a116cdbb9b0b9ca96787
DanyArctic <flip4477@gmail.com>
2020-04-02 21:39:48 +0300

    shortened code with operators and, or

--- ski_equipment/main.cpp
+++ ski_equipment/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+
 using namespace std; /*что бы не исп
  * std везде, но это не очень хорошо*/
 
@@ -7,36 +8,24 @@ int main() {
     std::cout << "Do you want to buy ski or snowboard?" << std::endl; //just comment
     std::string answer;
     std::cin >> answer;
-    if (answer == "ski")
-    {
-        std::cout << "loser! but okay, tell my your weight" << std::endl;
+    if (answer == "ski" || answer == "snowboard") {
+        std::cout << "okay, tell my your weight" << std::endl;
         int weight = 0;
         std::cin >> weight;
-        if (weight < 60)
-        {
-            std:cout << "then sise of your ski is S" << std::endl;
-        }
-        else
-        {
+        if (answer == "ski" && weight < 50) {
+            std::cout << "then sise of your ski is S" << std::endl;
+        } else if (answer == "ski" && weight > 70) {
             std::cout << "Probably your sise will be Large" << std::endl;
+        } else if (answer == "ski") {
+            std::cout << "then sise of your ski is Medium" << std::endl;
+        } else {
+            if (weight < 50) {
+                std::cout << "Your sise of board is S" << std::endl;
+            } else {
+                std::cout << "Go to GYM!" << std::endl;
+            }
         }
-    }
-    else if (answer == "snowboard")
-    {
-        std::cout << "yeah, boy!" << std::endl << "What's your weight then?" << std::endl;
-        int weight = 0;
-        std::cin >> weight;
-        if (weight < 50)
-        {
-            std::cout << "Your sise of board is S" << std::endl;
-        }
-        else
-        {
-            std::cout << "Go to GYM!" << std::endl;
-        }
-    }
-    else
-    {
+    } else {
         std::cout << "ничего не понимаю, что ты говоришь" << std::endl;
     }
 
