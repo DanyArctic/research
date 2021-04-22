c07da9b4354e7b0a175b650ab7b38cb0253eb333
DanyArctic <flip4477@gmail.com>
2021-04-22 20:46:13 +0300

    solved memory leak with using pointers

--- binary_search/find.h
+++ binary_search/find.h
@@ -2,37 +2,42 @@
 #include <iostream>
 #include <vector>
 #include <numeric>
+#include <memory>
 
-int find(std::vector<int> array, int target)
+int find(const std::vector<int> *array, int target)
 {
-    if (array.empty() || array.front() > array.back())
+    if (array == nullptr || array->empty() || array->front() > array->back())
     {
         return -1;
     }
-    int *min = new int(0);
-    int *max = new int(array.size() - 1);
-    int *middle = new int(std::midpoint(*min, *max)); //альтернатива (min+max)/2
-    int *cnt = new int(0);
-    bool *found = new bool(false);
-    while ((*min <= *max) && (*found != true))
+    std::unique_ptr<int> min(new int(0));
+    std::unique_ptr<int> max(new int(array->size() - 1));
+    std::unique_ptr<int> middle(new int(std::midpoint(*min, *max))); //альтернатива (min+max)/2
+    std::unique_ptr<int> cnt(new int(0));
+    std::unique_ptr<bool> found(new bool(false));
+    while (*min <= *max)
     {
         ++*cnt;
         *middle = std::midpoint(*min, *max);
-        if (array[*middle] < target)
+        if ((*array)[*middle] < target)
         {
             *min = *middle + 1;
         }
-        else if (array[*middle] > target)
+        else if ((*array)[*middle] > target)
         {
             *max = *middle - 1;
         }
         else //(array[*middle] == target)
         {
-            *found = true;
-            std::cout << "number of iterations is: " << *cnt << '\n'; //правильно ли считаются итерации? сложность?
-            return *middle;
+            std::cout << "number of iterations is: " << *cnt << '\n';
+            found = std::make_unique<bool>(true);
+            break;
         }
     }
-    std::cout << "not found" << '\n';
-    return -1;
+    if (!*found.get())
+    {
+        *middle = -1;
+        std::cout << "not found" << '\n';
+    }
+    return *middle;
 }
--- binary_search/tests.cpp
+++ binary_search/tests.cpp
@@ -4,53 +4,58 @@
 TEST(SearchTests, perfect_condition_search_works_fine_and_return_element_position_7)
 {
     std::vector<int> data{1,2,3,4,5,6,7,8,9,10};
-    EXPECT_EQ(7, find(data, 8)) << "simple search failed";
+    EXPECT_EQ(7, find(&data, 8)) << "simple search failed";
 }
 
 TEST(SearchTests, perfect_condition_search_works_fine_and_return_element_position_4_test_two)
 {
     std::vector<int> data{1,8,79,144,256,364,1720,2264,8795,9987129,123486761};
-    EXPECT_EQ(4, find(data, 256)) << "simple search failed";
+    EXPECT_EQ(4, find(&data, 256)) << "simple search failed";
 }
 
 TEST(SearchTests, perfect_condition_search_works_fine_and_return_element_position_5_test_tree)
 {
     std::vector<int> data{17,40,44,87,117,1024,11345,345234,3454366,6749679};
-    EXPECT_EQ(5, find(data, 1024)) << "simple search failed";
+    EXPECT_EQ(5, find(&data, 1024)) << "simple search failed";
 }
 
 TEST(SearchTests, searching_first_element_returns_0)
 {
     std::vector<int> data{1,2,3,5,6,7,8,9,10};
-    EXPECT_EQ(0, find(data, 1)) << "search of first element failed";
+    EXPECT_EQ(0, find(&data, 1)) << "search of first element failed";
 }
 
 TEST(SearchTests, searching_last_element_returns_9)
 {
     std::vector<int> data{1,2,3,4,5,6,7,8,9,10};
-    EXPECT_EQ(9, find(data, 10)) << "search of last element failed";
+    EXPECT_EQ(9, find(&data, 10)) << "search of last element failed";
 }
 
 TEST(SearchTests, searching_middle_element_returns_9)
 {
     std::vector<int> data{1,2,3,4,5,6,7,8,9,10,11};
-    EXPECT_EQ(5, find(data, 6)) << "search of middle element failed";
+    EXPECT_EQ(5, find(&data, 6)) << "search of middle element failed";
 }
 
 TEST(SearchTests, searching_non_specified_element_returns_minus_1)
 {
     std::vector<int> data{1,2,3,4,5,6,7,8,9,10};
-    EXPECT_EQ(-1, find(data, 11)) << "search of non specified number failure";
+    EXPECT_EQ(-1, find(&data, 11)) << "search of non specified number failure";
 }
 
 TEST(SearchTests, searching_non_specified_element_returns_minus_1_test_two)
 {
     std::vector<int> data{2,3,4,5,6,7,8,9,10,11};
-    EXPECT_EQ(-1, find(data, 1)) << "search of non specified number failure";
+    EXPECT_EQ(-1, find(&data, 1)) << "search of non specified number failure";
 }
 
 TEST(SearchTests, searching_non_specified_element_returns_minus_1_test_tree)
 {
     std::vector<int> data{1,3,4,5,6,7,8,9,10};
-    EXPECT_EQ(-1, find(data, 2)) << "search of non specified number failure";
+    EXPECT_EQ(-1, find(&data, 2)) << "search of non specified number failure";
+}
+
+TEST(SearchTests, search_in_nullptr_returns_minus)
+{
+    EXPECT_EQ(-1, find(nullptr, 2)) << "search in nullptr failure";
 }
