2711c3951b349fcb5bf824d4db12800b6682e783
DanyArctic <flip4477@gmail.com>
2022-01-30 11:21:55 +0300

    add exceptions

--- threading/threading.cpp
+++ threading/threading.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <chrono>
 #include <fstream>
+#include <string>
 
 void console_output_values(int i, double d, const std::string& s)
 {
@@ -26,42 +27,56 @@ int main()
 {
 	std::cout << "main thread id is: " << std::this_thread::get_id() << std::endl;
 	std::this_thread::sleep_for(std::chrono::milliseconds(500));
+	
 	std::thread thr(console_output_values, 1, 2.34, "example");
 	thr.join();
+	
 	int val = 7;
 	std::thread thr_two(increment_from_reference, std::ref(val));
 	//thr_two.detach();
 	thr_two.join();
 	std::cout << val << std::endl;
-
-	std::ifstream file("C:\\Users\\danik\\source\\repos\\research\\threading\\values.txt");
-	if (file.is_open())
+	std::string input_path = "C:\\Users\\danik\\source\\repos\\research\\threading\\input.txt";
+	std::string output_path = "C:\\Users\\danik\\source\\repos\\research\\threading\\output.txt";
+	std::ifstream file;
+	file.exceptions(std::ifstream::badbit | std::ifstream::failbit);
+	try
 	{
-		std::cout << "Okay! File opened\n\n" << std::endl;
+		file.open(input_path);
+		std::cout << "Okay! File opened" << std::endl;
 	}
-	else
+	catch (const std::ifstream::failure &ex)
 	{
-		std::cout << "File didn't open!\n\n" << std::endl;
-		return -1;
+		std::cerr << ex.what() << '\n';
+		std::cerr << ex.code() << '\n';
+		std::cerr << "Error: file didn't open! Values can't be read." << '\n';
 	}
 	static int a = 0, b = 0;
 	int result = 0;
 	file >> a >> b;
-	std::thread thr_three([&result]() {result = summary(std::ref(a), std::ref(b)); }); //why it works with static variables and how to capture int with lambdas(e.g. typical)?
-	thr_three.join();
-	std::cout << "result of summary = " << result << '\n';
-	//should I close file?
-	std::ofstream out("C:\\Users\\danik\\source\\repos\\research\\threading\\values.txt", std::ios::app);
 
-	if (out.is_open())
+	std::thread thr_three([&result]() {result = summary(std::ref(a), std::ref(b)); }); //why it works with static variables and how to capture with lambdas(e.g. typical int)?
+	thr_three.join();
+	
+	//should I close file?
+	std::ofstream out(output_path, std::ios::app); //new line in new file, if it doesn't exist
+	try
 	{
-		out << '\n' << result << std::endl;
+		if (out.is_open())
+		{
+			out << result << std::endl;
+		}
+		else
+		{
+			throw "File opening failure. Values can't be written.";
+		}
 	}
-	else
+	catch (const char* exception_message)
 	{
-		std::cout << "File opening failure. Process terminate." << std::endl;
-		return -1;
+		std::cerr << "Error: " << exception_message << '\n';
 	}
 	out.close();
+	std::cout << "result of summary = " << result << '\n';
+
 	return 0;
 }
